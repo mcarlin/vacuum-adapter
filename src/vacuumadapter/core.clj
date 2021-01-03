8e8fdc0881dda95abbc969b17f5f7ce3aac09aab
Mike Carlin <mikefcarlin@protonmail.com>
2021-01-03 06:03:35 -0800

    Add CLI to vacuum adapter

--- src/vacuumadapter/core.clj
+++ src/vacuumadapter/core.clj
@@ -1,6 +1,7 @@
 (ns vacuumadapter.core
   (:use [scad-clj.model])
-  (:use [scad-clj.scad]))
+  (:use [scad-clj.scad]),
+  (:require [cli-matic.core :refer [run-cmd]]))
 
 (defn tube
   [diameter height thickness]
@@ -27,5 +28,27 @@
       (translate [0 0 (/ height2 2)]))
     (tri d1 (+ (/ thickness 2) (/ (- d2 d1) 2)))))
 
-(spit "scadtest.scad",
-      (write-scad (with-fn 100 (adapter 37.8 30.0 45 30.0 3.0))))
+(defn generate-scad
+  [entry]
+  (let [{:strs [name from-dia from-len to-dia to-len thickness]} entry]
+    (spit (str name ".scad")
+          (write-scad (with-fn 100 (adapter to-dia to-len from-dia from-len thickness))))))
+
+(defn generate-all
+  [{:keys [config]}]
+  (dorun (map generate-scad config)))
+
+(def CONFIGURATION
+  {:app         {:command     "vacuum"
+                 :description "A command-line vacuum adapter scad generator"
+                 :version     "0.1.0"}
+   :commands    [{:command     "generate"
+                  :opts        [{:option  "config"
+                                 :as      "The configuration file of adapters to generate"
+                                 :type    :jsonfile
+                                 :require true}]
+                  :runs        generate-all}]})
+
+(defn -main
+  [& args]
+  (run-cmd args CONFIGURATION))
